081bb77029e84c3cd06904b06a215ff72467691c
Roman <romansanwow@gmail.com>
2020-05-04 13:07:28 +0300

    add minify-css

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+
+
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+
+gulp.task('minify-css', function() {
+    return gulp.src('./src/css/*.css')
+      .pipe(rename('style.min.css'))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dist'));
+  });
