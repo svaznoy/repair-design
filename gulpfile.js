72aac4e6ce25b74c82bc30e7b11ba21d12842bb2
Roman <romansanwow@gmail.com>
2020-05-07 10:53:11 +0300

    refact: add scss files to watch function gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ function bs() {
     });
     watch("./src/*.html").on('change', browserSync.reload);
     watch("./src/sass/**/*.sass" , serveSass);
+    watch("./src/sass/**/*.scss" , serveSass);
     watch("./src/js/*.js").on('change', browserSync.reload);
 }
 
@@ -30,7 +31,7 @@ function miniCss() {
 
 
   function serveSass() {
-    return src("./src/sass/**/*.sass")
+    return src("./src/sass/**/*.sass", "./src/sass/**/*.scss")
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
