51c92516d2a38c607145e6b4fb6f0e03feb78e53
Roman <romansanwow@gmail.com>
2020-05-13 23:24:04 +0300

    fix: add fix modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const toggleModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-      element.addEventListener('click', toggleModal);
-  });
 
-  closeBtn.addEventListener('click',toggleModal);
-});
+document.addEventListener('DOMContentLoaded', function(event) {
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn = document.querySelector('.modal__close');
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible');
+        }   
+    
+    modalBtn.forEach(element => {
+         element.addEventListener('click', switchModal);
+      });
+      closeBtn.addEventListener('click', switchModal);
+
+      window.onclick = function(e) {
+        if (e.target == modal) {
+          modal.classList.toggle('modal--visible');
+        }
+      };
+      
+      
+    });
+
+   
