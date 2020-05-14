98df44929e1912f478e4ce396287b57b7cf48d8c
Roman <romansanwow@gmail.com>
2020-05-14 12:13:10 +0300

    fix: main.js add close modal fo button esc and click out of scope

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-        }   
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  } 
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click',switchModal);
+  });
+  // Закрываю модальное окно через класс modal__close
+  closeBtn.addEventListener('click', switchModal);
+  // закрываю модальное окно при клике за область модального окна
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.classList.toggle('modal--visible');
+    }
+  }
+  // Удаляю modal--visible при нажатии клавиши esc 
+  document.onkeydown = function(event) {
+    if(event.key === "Escape" || event.key === "Esc" || event.key === 27) {
+      modal.classList.remove('modal--visible');
+    }
+  } 
+});
+
+
+
+
+
+
+
+
     
-    modalBtn.forEach(element => {
-         element.addEventListener('click', switchModal);
-      });
-      closeBtn.addEventListener('click', switchModal);
 
-      window.onclick = function(e) {
-        if (e.target == modal) {
-          modal.classList.toggle('modal--visible');
-        }
-      };
-      
-      
-    });
-
-   
+    
+    
+ 
