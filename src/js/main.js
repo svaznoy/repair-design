914a32b744aa8fa72f70bf4476da5f46b7d53a1d
Roman <romansanwow@gmail.com>
2020-05-15 00:08:31 +0300

    feat: add javascript close modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,32 @@
 
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   } 
+
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click',switchModal);
+//   });
+//   // Закрываю модальное окно через класс modal__close
+//   closeBtn.addEventListener('click', switchModal);
+//   // закрываю модальное окно при клике за область модального окна
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.classList.toggle('modal--visible');
+//     }
+//   }
+//   // Удаляю modal--visible при нажатии клавиши esc 
+//   document.onkeydown = function(event) {
+//     if(event.key === "Escape" || event.key === "Esc" || event.key === 27) {
+//       modal.classList.remove('modal--visible');
+//     }
+//   } 
+// });
+
 $(document).ready(function () {
     var modal = $('.modal'),
     modalBtn= $('[data-toggle=modal]'),
@@ -16,19 +44,6 @@ $(document).ready(function () {
     
 
 });
-
-
-
-
-
-
-	
-
-
-
-
-
-
 
 
 $(document).ready(function(){
