65dc9271c12d5246472489fd9258569bff0dbfc5
Roman <romansanwow@gmail.com>
2020-05-16 13:20:37 +0300

    feat: add product and swiper slider

--- src/js/main.js
+++ src/js/main.js
@@ -55,25 +55,28 @@ $(document).ready(function () {
 });
 
 
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
 
+    var nextBtn = $('.swiper-button-next');
+    var prevBtn = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    nextBtn.css('left', prevBtn.width() + 10 + bullets.width() + 10);
+    bullets.css('left', prevBtn.width() + 10);
+  });
 
 $(document).ready(function(){
   /**
