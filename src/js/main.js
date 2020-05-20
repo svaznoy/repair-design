65d20898e6f44591e1890dc7858cfe9fc0368ebf
Roman <romansanwow@gmail.com>
2020-05-20 12:06:34 +0300

    feat: add yandex map api and yandex script

--- src/js/main.js
+++ src/js/main.js
@@ -156,6 +156,7 @@ $(document).ready(function () {
       
       
     },
+
   });
 
 
@@ -165,6 +166,66 @@ $(document).ready(function () {
   $("[type=tel]").mask("+7 (000) 00-00-000", {
     placeholder: "+7 (___) __-__-___",
   });
+
+
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.757891, 37.637973],
+            zoom: 11
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Где тоут работаю',
+            balloonContent: 'Скоро опять на работу((('
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/smart.svg',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([
+          55.797669, 37.938750], {
+            hintContent: 'Закрыто на карантин))))',
+            balloonContent: 'Когда нибудь откроют!)))',
+            iconContent: '12'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/avatar.svg',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
+
+
 });
 
 
@@ -253,3 +314,6 @@ var wow = new WOW({
   resetAnimation: true, // reset animation on end (default is true)
 });
 wow.init();
+
+
+
