cbf9366d61c0714b1e77d38976338a053305f31e
svaznoy99 <romansanwow@gmail.com>
2020-05-26 17:06:43 +0300

    feat: add scroll for id and download image for slider 2

--- src/js/main.js
+++ src/js/main.js
@@ -63,7 +63,12 @@ $(document).ready(function () {
     }
   });
 
-  
+  $("#nav").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
 
 
   $().ready(function () {
